c00f00aa9ea200be503c020656768c97884df507
runglacare <49858430+runglacare@users.noreply.github.com>
2019-05-16 16:04:01 +0700

    edit1
    edit1

---
+++ api.php
@@ -0,0 +1,11 @@
+<?php
+
+$today = array(
+  'type' => 'text',
+  'today' => date('Y-m-d')
+);
+
+
+echo json_encode($today);
+
+?>
---
+++ setting.php
@@ -0,0 +1,6 @@
+<?php
+//LINE API Setting
+define('LINE_MESSAGE_CHANNEL_ID','');
+define('LINE_MESSAGE_CHANNEL_SECRET','');
+define('LINE_MESSAGE_ACCESS_TOKEN','');
+ ?>
